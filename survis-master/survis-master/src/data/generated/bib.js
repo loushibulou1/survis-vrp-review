a58318a3b013c66f7a83812eae18f97a97b9b95c
loushibulou1 <loushibulou1@gmail.com>
2026-05-13 03:06:37 +0800

    Add files via upload

---
+++ survis-master/survis-master/src/data/generated/bib.js
@@ -0,0 +1,128 @@
+﻿const generatedBibEntries = {
+    "Bai2021": {
+        "author": "Bai, Ruibin and Chen, Xinan and Chen, Zhi-Long and Cui, Tianxiang and Gong, Shuhui and He, Wentao and Jiang, Xiaoping and Jin, Huan and Jin, Jiahuan and Kendall, Graham and Li, Jiawei and Lu, Zheng and Ren, Jianfeng and Weng, Paul and Xue, Ning and Zhang, Huayan",
+        "doi": "10.1080/00207543.2021.2013566",
+        "journal": "International Journal of Production Research",
+        "keywords": "type:Modelling, type:Offline Optimisation",
+        "title": "Analytics and machine learning in vehicle routing research",
+        "type": "article",
+        "year": "2023"
+    },
+    "Bengio2021": {
+        "author": "Bengio, Yoshua and Lodi, Andrea and Prouvost, Antoine",
+        "doi": "10.1016/j.ejor.2020.07.063",
+        "journal": "European Journal of Operational Research",
+        "keywords": "type:Modelling",
+        "title": "Machine learning for combinatorial optimization: a methodological tour d'horizon",
+        "type": "article",
+        "year": "2021"
+    },
+    "Bogyrbayeva2024": {
+        "author": "Bogyrbayeva, Aigerim and Meraliyev, Merayrslan and Mustakhov, Taulekhan and Dauletbayev, Bissenbay",
+        "doi": "10.1109/TITS.2023.3334976",
+        "journal": "IEEE Transactions on Intelligent Transportation Systems",
+        "keywords": "type:Online Optimisation",
+        "title": "Machine Learning to Solve Vehicle Routing Problems: A Survey",
+        "type": "article",
+        "year": "2024"
+    },
+    "Cai2024": {
+        "author": "Cai, H. and Xu, P. and Tang, X. and Lin, G.",
+        "doi": "10.3390/electronics13163242",
+        "journal": "Electronics",
+        "keywords": "type:Modelling",
+        "title": "Solving the Vehicle Routing Problem with Stochastic Travel Cost Using Deep Reinforcement Learning",
+        "type": "article",
+        "year": "2024"
+    },
+    "Chen2019": {
+        "author": "Chen, Xinyun and Tian, Yuandong",
+        "booktitle": "NeurIPS",
+        "keywords": "type:Offline Optimisation",
+        "title": "Learning to Perform Local Rewriting for Combinatorial Optimization",
+        "type": "inproceedings",
+        "year": "2019"
+    },
+    "Gao2020": {
+        "author": "Gao, Lei and Chen, Mingxiang and Chen, Qichang and Luo, Ganzhong and Zhu, Nuoyi and Liu, Zhixin",
+        "keywords": "type:Offline Optimisation",
+        "title": "Learn to Design the Heuristics for Vehicle Routing Problem",
+        "type": "misc",
+        "year": "2020"
+    },
+    "Hottung2022": {
+        "author": "Hottung, Andre and Tierney, Kevin",
+        "doi": "10.1016/j.artint.2022.103786",
+        "journal": "Artificial Intelligence",
+        "keywords": "type:Offline Optimisation",
+        "title": "Neural large neighborhood search for routing problems",
+        "type": "article",
+        "year": "2022"
+    },
+    "Kool2019": {
+        "author": "Kool, Wouter and van Hoof, Herke and Welling, Max",
+        "booktitle": "ICLR",
+        "keywords": "type:Online Optimisation",
+        "title": "Attention, Learn to Solve Routing Problems!",
+        "type": "inproceedings",
+        "year": "2019"
+    },
+    "Kwon2020": {
+        "author": "Kwon, Yeong-Dae and Choo, Jinho and Kim, Byoungjip and Yoon, Iljoo and Gwon, Youngjune and Min, Seungjai",
+        "booktitle": "NeurIPS",
+        "keywords": "type:Online Optimisation",
+        "title": "POMO: Policy Optimization with Multiple Optima for Reinforcement Learning",
+        "type": "inproceedings",
+        "year": "2020"
+    },
+    "Lei2022": {
+        "author": "Lei, Kun and Guo, Peng and Wang, Yi and Wu, Xiao and Zhao, Wenchao",
+        "doi": "10.1016/j.neucom.2022.08.005",
+        "journal": "Neurocomputing",
+        "keywords": "type:Online Optimisation",
+        "title": "Solve routing problems with a residual edge-graph attention neural network",
+        "type": "article",
+        "year": "2022"
+    },
+    "Li2021": {
+        "author": "Li, Sirui and Yan, Zhongxia and Wu, Cathy",
+        "booktitle": "NeurIPS",
+        "keywords": "type:Online Optimisation",
+        "title": "Learning to Delegate for Large-scale Vehicle Routing",
+        "type": "inproceedings",
+        "year": "2021"
+    },
+    "Liu2022": {
+        "author": "Liu, Z. and Li, X. and Khojandi, A.",
+        "doi": "10.1016/j.tre.2022.102816",
+        "journal": "Transportation Research Part E",
+        "keywords": "type:Modelling",
+        "title": "The flying sidekick traveling salesman problem with stochastic travel time: A reinforcement learning approach",
+        "type": "article",
+        "year": "2022"
+    },
+    "Lu2021": {
+        "author": "Lu, Hao and Zhang, Xingwen and Yang, Shuang",
+        "booktitle": "ICLR",
+        "keywords": "type:Offline Optimisation",
+        "title": "A Learning-based Iterative Method for Solving Vehicle Routing",
+        "type": "inproceedings",
+        "year": "2021"
+    },
+    "Ma2021": {
+        "author": "Ma, Yining and Li, Jingwen and Cao, Zhiguang and Song, Wen and Zhang, Le and Chen, Zhenghua and Tang, Jing",
+        "booktitle": "NeurIPS",
+        "keywords": "type:Online Optimisation",
+        "title": "Learning to Iteratively Solve Routing Problems with Dual-Aspect Collaborative Transformer",
+        "type": "inproceedings",
+        "year": "2021"
+    },
+    "daCosta2020": {
+        "author": "da Costa, Paulo and Rhuggenaath, Jason and Zhang, Yingqian and Akcay, Alp",
+        "booktitle": "ACML",
+        "keywords": "type:Offline Optimisation",
+        "title": "Learning 2-opt Heuristics for the Traveling Salesman Problem via Deep Reinforcement Learning",
+        "type": "inproceedings",
+        "year": "2020"
+    }
+};
